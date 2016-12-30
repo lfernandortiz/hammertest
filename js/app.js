78dbe22c550dc47a80f676b0a55e66819611cd51
lfernandortiz <lfernandortiz@gmail.com>
2016-12-30 11:19:48 -0500

    Adicion de hammer al menu registrando eventos a body

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
 $(document).foundation()
+
+var menu =  document.getElementById("menu");
+var background = document.getElementById("background");
+
+var gestosBackground = new Hammer(background);
+gestosBackground.on('swipeleft', function(ev){
+	console.log(ev.type);
+	menu.classList.remove('active');
+
+});
+gestosBackground.on('swiperight', function(ev){
+	console.log(ev.type);
+	menu.classList.add('active');
+});
+
+gestosBackground.on('tap', function(ev){
+	console.log(ev.type);
+	
+	console.log("menu esta activo: " + menu.className.includes('active'));
+
+	if(menu.className.includes('active')){
+		menu.classList.remove('active');
+	}
+	 
+});
+
+
+
+
+//funcion de control para el inicio de la app
+function iniciar(){
+	console.log("test hammer.js");
+}
+window.addEventListener('load',iniciar,false);
