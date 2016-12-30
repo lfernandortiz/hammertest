16f36c838ba09195de37d414300e6a1b3417256a
lfernandortiz <lfernandortiz@gmail.com>
2016-12-30 14:50:28 -0500

    adicion de capa oscura como la de bancolombia

--- js/app.js
+++ js/app.js
@@ -1,30 +1,54 @@
 $(document).foundation()
 
 var menu =  document.getElementById("menu");
+var botonMenu =  document.getElementById("closemenu");
 var background = document.getElementById("background");
+var backgroundblack = document.getElementById("blackb");
+
 
 var gestosBackground = new Hammer(background);
 gestosBackground.on('swipeleft', function(ev){
 	console.log(ev.type);
 	menu.classList.remove('active');
+	backgroundblack.classList.remove('activeb');
 
 });
 gestosBackground.on('swiperight', function(ev){
 	console.log(ev.type);
 	menu.classList.add('active');
+	backgroundblack.classList.add('activeb');
 });
 
 gestosBackground.on('tap', function(ev){
-	console.log(ev.type);
-	
+	console.log(ev.type);	
 	console.log("menu esta activo: " + menu.className.includes('active'));
-
 	if(menu.className.includes('active')){
-		menu.classList.remove('active');
-	}
-	 
+		menu.classList.remove('active');	
+	}	 
 });
 
+var backgroundBlackH = new Hammer(backgroundblack);
+backgroundBlackH.on('tap press', function(ev){
+	console.log(ev.type);
+	menu.classList.remove('active');
+	backgroundblack.classList.remove('activeb');
+});
+backgroundBlackH.on('swipeleft', function(ev){
+	console.log(ev.type);
+	menu.classList.remove('active');
+	backgroundblack.classList.remove('activeb');
+
+});
+
+
+
+
+var gestosBotonMenu = new Hammer(botonMenu);
+gestosBotonMenu.on('tap', function(ev){
+	console.log("Evento lanzado por: " + ev.target.getAttribute('id'));
+	menu.classList.remove('active');
+	backgroundblack.classList.remove('activeb');
+});
 
 
 
